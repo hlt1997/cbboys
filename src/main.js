cd0d4ec7308243ec1c8c184c95e1424baf31f72f
hanxiansheng <2256010114@qq.com>
2020-10-14 21:53:27 +0800

    增加插件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import axios from "mint-ui"
+import "mint-ui/lib/style.min.css"
 
+Vue.use(MintUI)
+axios.defaults.baseURL="http://127.0.0.1:3000"
+Vue.protype.axios=axios;
 Vue.config.productionTip = false
 
 new Vue({
