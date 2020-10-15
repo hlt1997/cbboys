e9dc2333092d283a466e643510630a671baa8d36
dys <414193396@qq.com>
2020-10-15 22:34:15 +0800

    10.15晚上详情页

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-   
->>>>>>> e50a1a768bec0b7f42f5a7658826e7c7f5b1b587
->>>>>>> 8193f0e7f5c9ed06426357402dd2877700d1f694
+
+
     <router-view/>
   </div>
 </template>
 
-<<<<<<< HEAD
-=======
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,4 +16,4 @@
   color: #2c3e50;
 }
 </style>
->>>>>>> 8193f0e7f5c9ed06426357402dd2877700d1f694
+
