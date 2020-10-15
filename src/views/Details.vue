1979b8790950f0bf0184873b66d461a034a47709
dys <414193396@qq.com>
2020-10-15 22:24:40 +0800

    10.15号晚上详情页

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,17 +1,68 @@
 <template>
   <div class="details">
-    <!-- 顶部导航栏 -->
-    <mt-header>
-      <div slot='left'>
-        <router-link to='/'>
-          <mt-button icon='back'></mt-button>
-        </router-link>
-      </div>
-      <div solt='right'>
-        <router-link>
-          <button icon='more'></button>
-        </router-link>
-      </div>
-    </mt-header>
+    <header>
+      <!-- 顶部导航栏 -->
+      <mt-header>
+        <!-- 左边的返回键 -->
+        <div slot='left'>
+          <router-link to='/'>
+            <mt-button icon='back'></mt-button>
+          </router-link>
+        </div>
+        <!-- 右边链接键 -->
+        <div slot="right">
+          <!-- 分享链接 -->
+          <router-link to="/">
+            <img src="../../public/img/details-image/链接.png" alt="" class="img-header">
+          </router-link>
+          <!-- 购物车链接 -->
+          <router-link to="/shopcart">
+            <img src="../../public/img/details-image/购物车.png" class="img-header">
+          </router-link>
+          <!-- 更多链接 -->
+          <router-link to="/" class="img-header">
+            <mt-button icon="more" ></mt-button>
+          </router-link>
+        </div>
+      </mt-header>
+    </header>
+    <div>
+      <img src="../../public/img/detail/detail_img1.jpg" class="sideshow">
+    </div>
+    <div class="money">
+      <div class="price">55</div>
+      <span class="cotton">棉被</span>
+    </div>
   </div>
 </template>
+<style  scoped>
+.details{
+  background: #e8e8e8;
+}
+/* 顶部导航栏中img的外边距 */
+  .details .img-header{
+    margin: 0 10px;
+  }
+  .details .sideshow{
+    width: 100%;
+    height: 300px;
+  }
+  .details .money{
+    margin: 10px 10px;
+    background: #fff;
+    border-radius: 5px;
+  }
+  .details .price{
+    
+    padding: 10px;
+  }
+  .details .cotton{
+    padding: 5px 10px;
+    border-radius: 10px;
+    font-size: 12px;
+    background: #f00;
+    display: inline-block;
+    margin-left:10px ;
+    color: #FFF;
+  }
+</style>
